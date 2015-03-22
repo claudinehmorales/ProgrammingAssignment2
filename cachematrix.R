f50fd8712924ed3aedf4db947c584c9f3d2ce4ec
claudinehmorales <claudinehmorales@gmail.com>
2015-03-21 17:08:04 -0700

    Edited: Solution to Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -7,28 +7,28 @@
 
 ## Initialize the makeCacheMatrix() function, which only takes one matrix for an argument:
 makeCacheMatrix <- function(x = matrix()) { 
-      ## First, initialize the invrs variable:
-      invrs <- NULL 
-      ## Then the series of functions described above:
-      ## First, the set() function:
-      set <- function(y){ 
-          ## The purpose of the set() function is to assign the matrix passed in makeCacheMatrix()
-          ## to a variable y in the global environment using the '<<-' assignment operator:
-          x <<- y
-          ## The set() function also initializes the invrs variable in the global environment:
-          invrs <<- NULL
-      }
-      ## Next, the get() function, which returns the matrix passed in makeCacheMatrix():
-      get <- function() x
-      ## Then the setMatrixInverse() function, which returns invrs and assigns it to the variable inverse,
-      ## stored in the global environment. At this point, invrs will now be cached.
-      setMatrixInverse <- function(inverse) invrs <<- inverse
-      ## The getMatrixInverse() function returns the cached value of invrs.
-      getMatrixInverse <- function() invrs
-      ## Finally, a list of the above functions and their respective values:
-      list(set = set, get = get, setMatrixInverse = setMatrixInverse, 
-            getMatrixInverse = getMatrixInverse)
+        ## First, initialize the invrs variable:
+        invrs <- NULL 
+        ## Then the series of functions described above:
+        ## First, the set() function:
+        set <- function(y){ 
+              ## The purpose of the set() function is to assign the matrix passed in makeCacheMatrix()
+              ## to a variable y in the global environment using the '<<-' assignment operator:
+              x <<- y
+              ## The set() function also initializes the invrs variable in the global environment:
+              invrs <<- NULL
+        }
+        ## Next, the get() function, which returns the matrix passed in makeCacheMatrix():
+        get <- function() x
+        ## Then the setMatrixInverse() function, which returns invrs and assigns it to the variable inverse,
+        ## stored in the global environment. At this point, invrs will now be cached:
+        setMatrixInverse <- function(inverse) invrs <<- inverse
+        ## The getMatrixInverse() function returns the cached value of invrs:
+        getMatrixInverse <- function() invrs
+        ## Finally, a list of the above functions and their respective values:
+        list(set = set, get = get, setMatrixInverse = setMatrixInverse, getMatrixInverse = getMatrixInverse)
 }
+
 
 
 ## The cacheSolve() function calculates the inverse of a matrix that you you'd passed in to makeCacheMatrix()
@@ -37,23 +37,24 @@ makeCacheMatrix <- function(x = matrix()) {
 ## and stored previously, it does not go through the calculation at all. Rather, it returns the cached inverse. 
 ## If it has not been previously calculated and stored, it calculates it and then stores it.
 
-## Initialize cacheSolve() and pass a matrix you'd passed in to makeCacheMatrix() earlier:
+
+## Initialize cacheSolve() and which takes a matrix you'd passed in makeCacheMatrix() earlier for an argument:
 cacheSolve <- function(x) {
         ## Look through the cache to see if the inverse of your matrix has been stored:
         invrs <- x$getMatrixInverse()
         ## If it has been stored, return the cached invrs (function terminates, end of story):
         if(!is.null(invrs)){
-            message("Getting cached data...")
-            return(invrs)
+              message("This is cached data:")
+              return(invrs)
         }
         else {
-        ## If it has NOT been stored, the value of your matrix is assigned to variable newMatrix: 
-        newMatrix <- x$get()
-        ## Its inverse is then calculated:
-        invrs <- solve(newMatrix)
-        ## Then stored (so that if you want to get it again, no new expensive calculation will be made!):
-        x$setMatrixInverse(invrs)
-        ## Finally, it is returned to you:
-        invrs
+              ## If it has NOT been stored, the value of your matrix is assigned to the variable newMatrix: 
+              newMatrix <- x$get()
+              ## Its inverse is then calculated:
+              invrs <- solve(newMatrix)
+              ## And then stored (so that if you want to get it again, no new expensive calculation will be made!):
+              x$setMatrixInverse(invrs)
+              ## Finally, it is returned to you:
+              return(invrs)
         }
 }
